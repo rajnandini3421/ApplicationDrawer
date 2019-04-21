f4f1f0ade0f3a8630db8cb411e67ba0644c9f626
Rajnandini <rajnandinimahajan22@gmail.com>
2019-04-21 22:44:41 +0530

    Commit 21 April

---
+++ app/src/main/java/com/example/hp/applicationdrawer/FirstFragment.java
@@ -0,0 +1,22 @@
+package com.example.hp.applicationdrawer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HP on 21-04-2019.
+ */
+
+public class FirstFragment extends Fragment {
+    View MyView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        MyView = inflater.inflate(R.layout.first_layout, container, false);
+        return MyView;
+    }
+};
---
+++ app/src/main/java/com/example/hp/applicationdrawer/ThirdFragment.java
@@ -0,0 +1,22 @@
+package com.example.hp.applicationdrawer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HP on 21-04-2019.
+ */
+
+public class ThirdFragment extends Fragment {
+    View MyView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        MyView = inflater.inflate(R.layout.third_layout, container, false);
+        return MyView;
+    }
+};
